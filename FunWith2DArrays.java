7fa89a5124effb73493b63d1ce0a1f8a43d92357
evelyny2789 <evelyny2789@nycstudents.net>
2025-03-05 03:34:15 +0000

    commit

---
+++ FunWith2DArrays.java
@@ -0,0 +1,23 @@
+public class FunWith2DArrays {
+    public static int totalElements(int[][] array){
+        int rows = array.length; 
+        int columns = array[0].length; 
+        return rows * columns;
+    }
+
+    public static void fourCorners(String[][] array){
+        int rows = array.length; 
+        int columns = array[0].length;
+
+        System.out.println(array[0][0]); 
+        System.out.println(array[0][columns - 1]); 
+        System.out.println(array[rows - 1][0]); 
+        System.out.println(array[rows - 1][columns - 1]); 
+    }
+
+    public static void swapFrontAndBackRows(String[][] array){
+        String[] firstRow = array[0]; 
+        array[0] = array[array.length - 1]; 
+        array[array.length - 1] = firstRow; 
+    }
+}
